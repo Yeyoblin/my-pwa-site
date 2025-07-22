efd754b2fb9c75e90b23a5a1e8d196e9668387da
Yeyoblin <sun_beautiful@list.ru>
2025-07-22 17:04:22 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,9 +3,14 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/styles.css',
-  '/script.js',
+  '/fairies.html',
+  '/herbalism.html',
+  '/contacts.html',
+  '/outdoor.html',
+  '/submit_form.php',
   '/icon-192x192.png',
-  '/icon-512x512.png'
+  '/icon-512x512.png',
+
 ];
 
 self.addEventListener('install', (event) => {
@@ -19,5 +24,25 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Если файл есть в кеше — вернём его
+        if (response) {
+          return response;
+        }
+        // Если нет — загрузим из сети и добавим в кеш
+        return fetch(event.request).then((response) => {
+          // Клонируем ответ, т.к. он может быть использован только один раз
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME)
+            .then((cache) => cache.put(event.request, responseToCache));
+          return response;
+        });
+      })
   );
 });
